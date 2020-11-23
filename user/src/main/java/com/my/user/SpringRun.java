7729d7320b9dd2c14200ef1cf3a6f68c047f532c
mengyufei <mengyufei@snsoft.com.cn>
2020-11-23 18:03:24 +0800

    项目

---
+++ user/src/main/java/com/my/user/SpringRun.java
@@ -0,0 +1,13 @@
+package com.my.user;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRun {
+
+    public static void main(String[] args) {
+       SpringApplication.run(SpringRun.class,args);
+    }
+}
