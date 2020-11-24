d03aec97e1e184c74e29dc26815a84402508ee40
mengyufei <mengyufei@snsoft.com.cn>
2020-11-24 15:01:37 +0800

    注册中心

--- user/src/main/java/com/my/user/SpringRun.java
+++ user/src/main/java/com/my/user/SpringRun.java
@@ -3,8 +3,10 @@ package com.my.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringRun {
 
     public static void main(String[] args) {
